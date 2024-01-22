c7a57d19ed052456977f9499dd2b6cd0d4354945
tjdwns335 <http://github.com/tjdwns335>
2024-01-22 11:45:55 +0900

    1교시

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,57 @@
-import { useState } from "react";
-import "App.css";
+import React, { useState } from 'react'
 
 function App() {
-  const initialArray = ["apple", "banana", "cherry", "elderberry", "watermelon", "grape",];
+  const initialArray = ['apple', 'banana', 'cherry', 'elderberry', 'watermelon', 'grape'];
 
   const [array, setArray] = useState(initialArray);
   const [result, setResult] = useState("");
   const [query, setQuery] = useState("");
 
-  /** 함수들 */
+  // 함수들
   const handleForEach = () => {
     let tempResult = "";
-
-    array.forEach(function (fruit) {
-      tempResult += `${fruit} ,`;
+    array.forEach((fruit) => {
+      tempResult += `${fruit}, `;
     });
-
+    // tempResult = tempResult.slice(0, -2);
     setResult(tempResult.slice(0, -2));
-  };
+  }
 
   const handleFilter = () => {
-    const filteredList = array.filter(function (fruit) {
-      // 얘를 필터링을 할지 말지를 결정한다.
-      if (fruit.includes(query)) {
-        return true;
-      } else {
-        return false; // 여기서 결정한다.
-      }
+    const filteredList = array.filter((fruit) => {
+      return fruit.includes(query) ? true : false;
     });
-    setResult(filteredList.join(", "));
-  };
+    setResult(filteredList.join(', '));
+  }
 
   const handleMap = () => {
-    // array를 대문자로 변환하여 출력
-    const mappedList = array.map(function (fruit) {
+    const mappedList = array.map((fruit) => {
       return fruit.toUpperCase();
-    });
-    setResult(mappedList.join(", "));
-  };
-
-  const handleReduce = () => {
-    const reducedListText = array.reduce(function (acc, cur) {
-      return `${acc}, ${cur}`;
-    });
-    setResult(reducedListText);
-  };
-
-  const handlePush = () => {
-    if (!query) {
-      alert("값이 없습니다!");
-      return false;
-    }
-
-    // input 태그에 입력한 값이 결과의 끝에 달라붙도록!
-    const newArr = [...array, query];
-    setArray(newArr);
-    setResult(newArr.join(", "));
-  };
-
-  const handlePop = () => {
-    const newArr = [...array];
-    newArr.pop();
-    setArray(newArr);
-    setResult(newArr.join(", "));
-  };
+    })
+    setResult(mappedList.join(', '));
+  }
 
   return (
-    <div className="test">
-      <h1>Standard반 배열 API</h1>
+    <div>
+      <h1>Array Api practice</h1>
       <div>
         <input
+          type="text"
           value={query}
-          onChange={function (e) {
+          onChange={(e) => {
             setQuery(e.target.value);
           }}
         />
       </div>
       <div>
         <button onClick={handleForEach}>forEach</button>
-        <button onClick={handleFilter}>filter</button>
-        <button onClick={handleMap}>map</button>
-        <button onClick={handleReduce}>reduce</button>
-        <button onClick={handlePush}>push</button>
-        <button onClick={handlePop}>pop</button>
+        <button onClick={handleFilter}>Filter</button>
+        <button onClick={handleMap}>Map</button>
       </div>
-      <div>
-        <strong>원본 배열</strong> : {array.join(", ")}
-      </div>
-      <div>
-        <strong>결과물</strong> : {result}
-      </div>
+      <div><strong>Array</strong> : {array.join(', ')} </div>
+      <div><strong>Result</strong> : {result} </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
